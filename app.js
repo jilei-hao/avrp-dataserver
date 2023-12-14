667844fac369c4b6892fc5ffc3c23714527e9d1a
jilei-hao <jilei.cs@gmail.com>
2023-12-14 16:29:42 -0500

    minor change on indentation

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ const app = express();
 
 // Configure CORS middleware
 const corsOptions = {
-    origin: 'http://localhost:3030', // Replace with your allowed origin(s)
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, // enable set cookie with CORS
-    optionsSuccessStatus: 204,
+  origin: 'http://localhost:3030', // Replace with your allowed origin(s)
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true, // enable set cookie with CORS
+  optionsSuccessStatus: 204,
 };
 
 app.use(cors(corsOptions));
@@ -19,5 +19,5 @@ app.use('/data', dataRoute);
 // Start the Express server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
